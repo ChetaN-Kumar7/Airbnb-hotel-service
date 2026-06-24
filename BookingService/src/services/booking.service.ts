06ccf45e64643f9fbf25af7121d34c133ed294b5
ChetaN-Kumar7 <chetankumar3018@gmail.com>
2026-06-24 08:10:41 +0530

    Added txn and locks to the idempotent apis

--- BookingService/src/services/booking.service.ts
+++ BookingService/src/services/booking.service.ts
@@ -1,7 +1,44 @@
-export  async function createBookingService() {
-    
+import  PrismaClient from "../prisma/client";
+import { CreateBookingDto } from "../dto/booking.dto";
+import { confirmBooking, createBooking, createIdempotencyKey, finalizeIdempotencyKey, getIdempotencyKeyWithLock } from "../repositries/booking.repository";
+import { BadRequestError, NotFoundError } from "../utils/errors/app.error";
+import { generateIdempotencyKey } from "../utils/generateIdempotencyKey";
+
+export  async function createBookingService(createBookingDto: CreateBookingDto) {
+    const booking = await createBooking({
+        userId:createBookingDto.userId,
+        hotelId:createBookingDto.hotelId,
+        bookingAmount : createBookingDto.bookingAmount,
+        totalGuests : createBookingDto.totalGuests
+    })
+
+    const idempotencyKey = generateIdempotencyKey();
+
+    await createIdempotencyKey(idempotencyKey,booking.id);
+
+    return {
+        bookingId: booking.id,
+        idempotencyKey : idempotencyKey,
+    }
 }
 
-export async function finalizeBookingService() {
-    
+export async function confirmBookingService(idempotencyKey: string) {
+
+    return await PrismaClient.$transaction( async(tx)=>{
+        const idempotencyKeyData = await getIdempotencyKeyWithLock(tx,idempotencyKey)
+
+        if(!idempotencyKeyData || !idempotencyKeyData.bookingId){
+            throw new NotFoundError('Idempotency key not found');
+        }
+
+        if(idempotencyKeyData.finalized){
+            throw new BadRequestError('Idempotency key already finalized');
+        }
+
+        const  booking = await confirmBooking(tx,idempotencyKeyData.bookingId);
+        await finalizeIdempotencyKey(tx,idempotencyKey)
+
+        return booking;
+    })
+
 }
